6b00ac98e7c4fafb31b588b6f8c35a3c90684f4b
Victor Lopez <vlopezjr@yahoo.com>
2019-05-15 13:05:29 -0700

    Created library.

---
+++ cplibrary/Utility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cplibrary
+{
+    public static class Utility
+    {
+        public static string Version
+        {
+            get
+            {
+                return "Version 1.0.0";
+            }
+        }
+    }
+}
